19fed3ee127897a7889349ae7aeb3fe36be7eed3
Mario Cueva <mario.cueva.au@gmail.com>
2017-09-13 22:34:04 +1000

    completed challenge

--- DNA-Pairing.js
+++ DNA-Pairing.js
@@ -11,12 +11,24 @@ For example, for the input GCG, return [["G", "C"], ["C","G"],["G", "C"]]
 The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
 */
 
-function pairElement(str) {
-    return str;
-  }
-  
-console.log(pairElement("GCG")); //should return [["G", "C"], ["C","G"],["G", "C"]]
+//AT TA CG GC
 
+function pairElement(str) {
+
+    var arr = [];
+
+    for (var i = 0; i < str.length; i++) {
+        arr[i] = [str[i]];
+
+       if (arr[i] == "G") arr[i].push("C");
+       if (arr[i] == "C") arr[i].push("G");
+       if (arr[i] == "A") arr[i].push("T");
+       if (arr[i] == "T") arr[i].push("A");
+    }
+    return arr;
+}
+
+console.log(pairElement("GCG")); //should return [["G", "C"], ["C","G"],["G", "C"]]
 console.log(pairElement("ATCGA")); // should return [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]].
 console.log(pairElement("TTGAG")); // should return [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]].
 console.log(pairElement("CTCTA")); // should return [["C","G"],["T","A"],["C","G"],["T","A"],["A","T"]].
