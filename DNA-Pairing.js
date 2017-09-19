4592c05e89c7c6d59c2696815a01d4ab5f282977
Mario Cueva <mario.cueva.au@gmail.com>
2017-09-19 19:13:44 +1000

    commented inn an alternate solution not written by me but pretty cool

--- DNA-Pairing.js
+++ DNA-Pairing.js
@@ -32,3 +32,21 @@ console.log(pairElement("GCG")); //should return [["G", "C"], ["C","G"],["G", "C
 console.log(pairElement("ATCGA")); // should return [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]].
 console.log(pairElement("TTGAG")); // should return [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]].
 console.log(pairElement("CTCTA")); // should return [["C","G"],["T","A"],["C","G"],["T","A"],["A","T"]].
+
+
+/* alternate solution not written by me but pretty cool
+
+function pairElement(str) {
+  var pairs = {
+    "A": ["A", "T"],
+    "T": ["T", "A"],
+    "C": ["C", "G"],
+    "G": ["G", "C"]
+  };
+  
+  return str.split("").map(function(elem) {
+    return pairs[elem];
+  });
+}
+
+*/
